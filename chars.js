683f65188e7dba4e71f7e8db512ecb0c94833ede
marcosgil1996 <marcosgil1996@gmail.com>
2022-12-12 07:53:49 +0100

    Comentando código y cosas

--- chars.js
+++ chars.js
@@ -25,31 +25,14 @@ const $grafica = document.querySelector("#grafica");
 const allYears = new Array();
 const totalPoblacion = new Array();
 
+// Función a definir cada uno de los Chars
 function drawCharts() {
     let cont = 0;
     for(let i=2005; i<=2021; i++){
         cont ++;
         let tipoChar = "p" + cont + "Chart"; // Defiendo el nombre de char a seguir
-        drawChartPoblacion(i,tipoChar,"POBLACIÓN EN JÁVEA");
+        drawChartPoblacion(i,tipoChar,"POBLACIÓN EN JÁVEA "+i);
     }
-    /*
-    drawChartPoblacion("2021","p1Chart","POBLACIÓN JÁVEA 2021");
-    drawChartPoblacion("2020","p2Chart","POBLACIÓN JÁVEA 2020");
-    drawChartPoblacion("2019","p3Chart","POBLACIÓN JÁVEA 2019");
-    drawChartPoblacion("2018","p4Chart","POBLACIÓN JÁVEA 2018");
-    drawChartPoblacion("2017","p5Chart","POBLACIÓN JÁVEA 2017");
-    drawChartPoblacion("2016","p6Chart","POBLACIÓN JÁVEA 2016");
-    drawChartPoblacion("2015","p7Chart","POBLACIÓN JÁVEA 2015");
-    drawChartPoblacion("2014","p8Chart","POBLACIÓN JÁVEA 2014");
-    drawChartPoblacion("2013","p9Chart","POBLACIÓN JÁVEA 2013");
-    drawChartPoblacion("2012","p10Chart","POBLACIÓN JÁVEA 2012");
-    drawChartPoblacion("2011","p11Chart","POBLACIÓN JÁVEA 2011");
-    drawChartPoblacion("2010","p12Chart","POBLACIÓN JÁVEA 2010");
-    drawChartPoblacion("2009","p13Chart","POBLACIÓN JÁVEA 2009");
-    drawChartPoblacion("2008","p14Chart","POBLACIÓN JÁVEA 2008");
-    drawChartPoblacion("2007","p15Chart","POBLACIÓN JÁVEA 2007");
-    drawChartPoblacion("2006","p16Chart","POBLACIÓN JÁVEA 2006");
-    drawChartPoblacion("2005","p17Chart","POBLACIÓN JÁVEA 2005");*/
 }
 
 // Función para la creación de la gráfica
@@ -104,7 +87,7 @@ function drawChartPoblacion(fecha,numGrafico,TituloGrafica) {
 
 
 // Podemos tener varios conjuntos de datos. Comencemos con uno
-const datosVentas2020 = {
+const datosPoblacion = {
     label: "EMPADRONAMIENTO TOTAL",
     data: totalPoblacion, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
     backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
@@ -116,9 +99,7 @@ new Chart($grafica, {
     data: {
         labels: allYears,
         datasets: [
-            datosVentas2020,
-            // Aquí más datos...
-            
+            datosPoblacion,            
         ]
     },
     options: {
